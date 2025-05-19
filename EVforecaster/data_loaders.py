4ffd4e5b36e4193ec44321cc945985f0301b9993
Andriy Sinclair <andriysinclair@outlook.com>
2025-05-19 14:53:52 +0100

    Added Day data loader and merger function

--- EVforecaster/data_loaders.py
+++ EVforecaster/data_loaders.py
@@ -5,6 +5,7 @@ import pickle
 
 # Obtaining relevant paths
 trip_data = cfg.root_folder + "/data/trip_eul_2002-2023.tab"
+day_data = cfg.root_folder + "/data/day_eul_2002-2023.tab"
 
 # Set up basic configuration for logging
 logging.basicConfig(level=logging.DEBUG)
@@ -85,9 +86,10 @@ def trip_data_loader(trip_path, survey_years, chunksize,
         # Mapping...
         merged_df["TripType"] = merged_df["TripType"].map(trip_type_mapping)
 
-        # Dropping legact columns
+        # Renaming purpouse columns
 
-        merged_df = merged_df.drop(columns=["TripPurpFrom_B01ID", "TripPurpTo_B01ID"], axis=1)
+        merged_df = merged_df.rename(columns={"TripPurpFrom_B01ID": "TripStartLoc",
+                                              "TripPurpTo_B01ID": "TripEndLoc"})
 
         # Sort values to a rational order
 
@@ -100,6 +102,13 @@ def trip_data_loader(trip_path, survey_years, chunksize,
 
         logging.info(f"File saved to {output_file_name}")
 
+        for col in merged_df.columns:
+            logging.debug(col)
+
+        print("")
+
+        logging.debug(merged_df.dtypes)
+
         return merged_df
     
     if is_loaded:
@@ -110,23 +119,101 @@ def trip_data_loader(trip_path, survey_years, chunksize,
             merged_df = pickle.load(f)   
 
         logging.info(f"loading {output_file_name}")
+    
+        for col in merged_df.columns:
+            logging.debug(col)
+
+        print("")
+
+        logging.debug(merged_df.dtypes)
+
+        return merged_df
+    
+def day_data_loader(day_path, 
+                     output_file_name, 
+                     day_cols_to_keep = cfg.day_cols_to_keep, 
+                     is_loaded = False) -> pd.DataFrame:
+    
+    # Keeping only necessary cols
+
+    if not is_loaded:
+
+        day_df = pd.read_csv(day_path, sep="\t")
+
+        day_df = day_df[day_cols_to_keep]
+
+        # Converting day_ID to float for better merge
+        day_df["DayID"] = day_df["DayID"].astype(float)
+
+        logging.debug(day_df.dtypes)
+
+        with open(cfg.root_folder + f"/dataframes/{output_file_name}", "wb") as f:
+            pickle.dump(day_df, f)   
+
+        logging.debug(day_df.dtypes)
+
+        logging.info(f"File saved to {output_file_name}")
+
+        return day_df
+    
+    if is_loaded:
+
+        with open(cfg.root_folder + f"/dataframes/{output_file_name}", "rb") as f:
+            day_df = pickle.load(f)   
+
+        logging.debug(day_df.dtypes)
+
+        logging.info(f"loading {output_file_name}")
+
+        return day_df
+    
+def merge_dfs(df1, df2, common_id, output_file_name, is_loaded=False):
+
+    if not is_loaded:
+        df1 = df1.copy()
+        df2 = df2.copy()
+
+        merged_df = pd.merge(left=df1, right=df2, on=common_id)
+
+        with open(cfg.root_folder + f"/dataframes/{output_file_name}", "wb") as f:
+            pickle.dump(merged_df, f)   
+
+        logging.info(f"File saved to {output_file_name}")
+
+    else:
+        with open(cfg.root_folder + f"/dataframes/{output_file_name}", "rb") as f:
+            merged_df = pickle.load(f)  
+
+        logging.info(f"loading {output_file_name}")
 
         return merged_df
 
 def create_charging_nodes(trip_df):
-    pass
+    
+    df = trip_df.copy()
+
+    # For each individual fill in missing travel weeks
+
+
 
 if __name__ == "__main__":
     print(cfg.root_folder)
     print("")
     trip_df = trip_data_loader(trip_path=trip_data, survey_years=[2017], 
-                               chunksize=100000, output_file_name="trip_df_2017.pkl", is_loaded=False)
+                               chunksize=100000, output_file_name="trip_df_2017.pkl", is_loaded=True)
+    
+    day_df = day_data_loader(day_path=day_data,
+                             output_file_name="day_df.pkl",
+                             is_loaded=True)
 
+    
     trip_df.to_csv(cfg.root_folder + "/output_csvs/trip_df_2017.csv", index=False)
 
-    for col in trip_df.columns:
-        logging.info(col)
+    trip_df.head()
 
-    print("")
+    print(len(day_df))
+    print(day_df.head())
 
-    print(trip_df.dtypes)
+    day_trip_merge = merge_dfs(df1=trip_df, df2=day_df, common_id="DayID", output_file_name="day_trip_merge.pkl", is_loaded=True)
+
+    day_trip_merge.to_csv(cfg.root_folder + "/output_csvs/day_trip_merge_2017.csv", index=False)
