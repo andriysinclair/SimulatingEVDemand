7c26c099bbf313556a88bbca30e67159234a5ea2
Andriy Sinclair <andriysinclair@outlook.com>
2025-05-20 17:45:27 +0100

    Streamlined the charging logic, I think I have something half-working, I need to figure out how to make this into a time series...

--- EVforecaster/charging_logic.py
+++ EVforecaster/charging_logic.py
@@ -3,6 +3,7 @@ import config as cfg
 import random
 import numpy as np
 import logging
+from charging_logic_auxillary import generate_charger, obtain_decision_to_charge, calculate_charging_session
 
 # Set up basic configuration for logging
 logging.basicConfig(level=logging.INFO)
@@ -11,20 +12,6 @@ logging.basicConfig(level=logging.INFO)
 
 full_df_path = cfg.root_folder + "/dataframes/day_trip_merge.pkl"
 full_df = pd.read_pickle(full_df_path)
-
-
-def generate_charger(x, home_charger_likelihood=0.96, work_charger_likelihood=0.62, public_charger_likelihood=0.17):
-    if x == 3:
-        return random.choices([1,0], weights=[home_charger_likelihood,1-home_charger_likelihood])[0]
-    if x == 2:
-        return random.choices([1,0], weights=[public_charger_likelihood,1-public_charger_likelihood])[0]
-    else:
-        return random.choices([1,0], weights=[work_charger_likelihood,1-work_charger_likelihood])[0]
-    
-
-### Auxillary functions for charging logic ###
-
-
 
 
 def charging_logic(df, battery_size = cfg.battery_size, energy_efficiency=cfg.energy_efficiency,
@@ -47,21 +34,23 @@ def charging_logic(df, battery_size = cfg.battery_size, energy_efficiency=cfg.en
                          "TotalPowerUsed": [],
                          "ChargeStart": [],
                          "ChargeEnd": [],
+                         "ChargeDuration": [],
                          "ChargeLoc": [],
                          "ChargingRate": []}
+        
+        total_trips = 0
+        negative_trips = 0
  
         for i in individual_ids[:test_index]:
 
             i_df = df[df["IndividualID"]==i]
-
             i_df = i_df.copy()
 
             # Setting initial SOC uniformly distributed
 
             first_trip_req_charge = (i_df.iloc[0]["TripDisExSW"]* energy_efficiency)/1000
             init_SOC = random.uniform(first_trip_req_charge, battery_size)
-
-            logging.info(f"Intitial SOC: {init_SOC}")
+            logging.debug(f"Intitial SOC: {init_SOC}")
 
             # Calculating time at trip end location
 
@@ -79,194 +68,97 @@ def charging_logic(df, battery_size = cfg.battery_size, energy_efficiency=cfg.en
             charge_decision_list = []
             charge_start_time_list = []
             charge_end_time_list = []
+            charge_duration_list = []
             total_power_used_list = []
 
             for idx, row in i_df.iterrows():
 
-                # For all rows but the last
+                # Obtain all relevant parameters from that trip
+                available_charger = row["IsCharger"]
+                end_location = row["TripEndLoc"]
+                charging_rate = charging_rates[row["TripEndLoc"]]
+                time_duration_at_location = row["TimeEndLoc"]
+                time_at_location = row["TripEnd"]
+                current_SOC = SOC_list[-1]
+                charge_for_next_trip = row["Req_charge+1"]
 
-                if idx != i_df.index[-1]:
+                if idx == i_df.index[-1]:
+                    last_trip_flag = True
+                else:
+                    last_trip_flag = False
+                
+                
+                logging.debug(f"Current SOC: {current_SOC}")
+                logging.debug(f"charge required for next trip: {charge_for_next_trip}")
 
-                    # No charger available or insufficient stopping time
+                total_trips += 1
+                
+                if current_SOC < 0:
+                    negative_trips += 1
 
-                    if row["IsCharger"] == 0 or row["TimeEndLoc"] < min_stop_time_to_charge:
-                        
-                        SOC_list.append(    np.round(  SOC_list[-1]-row["Req_charge+1"],  2))
-                        charge_decision_list.append(0)
-                        logging.debug("No charger or not enough parking time")
-                        logging.debug(charge_decision_list)
-                        logging.debug(SOC_list)
+                # Obtain individuals decision to charge
+
+                decision_to_charge = obtain_decision_to_charge(SOC=current_SOC, available_charger=available_charger,
+                                                               time_duration_at_location=time_duration_at_location,
+                                                               last_trip_flag=last_trip_flag,
+                                                               min_stop_time_to_charge=cfg.min_stop_time_to_charge,
+                                                               battery_size=cfg.battery_size,
+                                                               SOC_charging_prob=cfg.SOC_charging_prob)
+                
+                charge_decision_list.append(decision_to_charge)
+
+                if decision_to_charge == 1:
                     
-                    else:
-                        logging.debug("charger is available and car has stopped for sufficient time")
-                        
-                        SOC_percentage = SOC_list[-1]/battery_size
-                        charge_decision_prob = SOC_charging_prob(SOC_percentage)
-                        charge_decision = np.random.choice([0,1], p = [1-charge_decision_prob, charge_decision_prob])
-                        charge_decision_list.append(charge_decision)
+                    new_SOC, total_power_used, charge_start_time, charge_end_time, charge_duration = calculate_charging_session(SOC=current_SOC, location_charging_rate=charging_rate,
+                                                                                                                                time_duration_at_location=time_duration_at_location,
+                                                                                                                                charge_start_time=time_at_location, last_trip_flag=last_trip_flag,
+                                                                                                                                battery_size=cfg.battery_size)
+                    
+                    # Populate dictionary
+                    charging_dict["IndividualID"].append(i)
+                    charging_dict["TotalPowerUsed"].append(total_power_used)
+                    charging_dict["ChargeStart"].append(charge_start_time)
+                    charging_dict["ChargeEnd"].append(charge_end_time)
+                    charging_dict["ChargeDuration"].append(charge_duration)
+                    charging_dict["ChargingRate"].append(charging_rate)
+                    charging_dict["ChargeLoc"].append(end_location)
 
-                        if charge_decision == 1:
+                    # Removing charge required for next trip
+                    if not last_trip_flag:
+                        new_SOC -= charge_for_next_trip
 
-                            charging_dict["IndividualID"].append(i)
-
-
-                            location_charging_rate = charging_rates[row["TripEndLoc"]]
-                            logging.debug(f"Location charging rate: {location_charging_rate}")
-
-                            # If we have a charge...
-                            logging.debug(f"Individual has chosen to charge")
-                            total_possible_charge = (row["TimeEndLoc"]/60) * location_charging_rate
-                            logging.debug(f"Total possible charge: {total_possible_charge}")
-
-                            if total_possible_charge > (battery_size - SOC_list[-1]):
-                                # Fully charge car
-                                logging.debug(f"As total possible charge: {total_possible_charge} > battery size: {battery_size}")
-                                logging.debug(f"Fully charging car")
-
-                                total_power_used = battery_size - SOC_list[-1]
-
-                                charging_dict["TotalPowerUsed"].append(total_power_used)
-
-                                charge_start_time = row["TripEnd"]
-                                charge_end_time = charge_start_time + (total_power_used/location_charging_rate*60)
-
-                                # Rounding to nearest 5 minutes
-                                charge_end_time = 5 * round(charge_end_time / 5)
-
-                                charge_start_time_list.append(charge_start_time)
-                                charge_end_time_list.append(charge_end_time)
-                                total_power_used_list.append(total_power_used)
-
-                                SOC_list.append(np.round(battery_size,2))
-
-                                # Removing SOC from next trip
-                                SOC_list[-1] = np.round(  SOC_list[-1]-row["Req_charge+1"],  2)
-
-                                logging.debug(charge_decision_list)
-                                logging.debug(SOC_list)
-
-
-                            else:
-                                
-                                # total power used = total posible charge
-
-                                charging_dict["TotalPowerUsed"].append(total_possible_charge)
-
-                                charge_start_time = row["TripEnd"]
-                                charge_end_time = charge_start_time + (total_possible_charge/location_charging_rate*60)
-
-                                # Rounding to nearest 5 minutes
-                                charge_end_time = 5 * round(charge_end_time / 5)
-
-                                charge_start_time_list.append(charge_start_time)
-                                charge_end_time_list.append(charge_end_time)
-                                total_power_used_list.append(total_possible_charge)
-
-                                SOC_list.append(np.round(   SOC_list[-1] + total_possible_charge, 2))
-
-                                # Removing SOC from next trip
-                                SOC_list[-1] = np.round(  SOC_list[-1]-row["Req_charge+1"],  2)
-
-                                logging.debug("Applying maximum possible charge")
-                                logging.debug(charge_decision_list)
-                                logging.debug(SOC_list)
-
-                            charging_dict["ChargeStart"].append(charge_start_time)
-                            charging_dict["ChargeEnd"].append(charge_end_time)
-                            charging_dict["ChargeLoc"].append(row["TripEndLoc"])
-                            charging_dict["ChargingRate"].append(location_charging_rate)
-                        
-                        else:
-
-                            # Removing SOC from next trip
-                            SOC_list.append(  np.round(  SOC_list[-1]-row["Req_charge+1"],  2) )
-                            logging.debug(f"Individual has chosen NOT to charge")
-                            logging.debug(charge_decision_list)
-                            logging.debug(SOC_list)
-
-
+                    charge_start_time_list.append(charge_start_time)
+                    charge_end_time_list.append(charge_end_time)
+                    charge_duration_list.append(charge_duration)
+                    total_power_used_list.append(total_power_used)
 
                 else:
-                    # For the final row
-                    logging.debug(f"Final row so no next trip")
 
-                    if row["IsCharger"] == 0:
-                        logging.debug(f"No charger available")
+                    if not last_trip_flag:
+                        new_SOC = current_SOC -  charge_for_next_trip
+                    
+                    
 
-                        charge_decision_list.append(0)
-                        SOC_list.append(np.round( SOC_list[-1], 2))
-                        logging.debug(charge_decision_list)
-                        logging.debug(SOC_list)
+                # Removing next trip from SOC
+                
+                SOC_list.append(new_SOC)
 
+                logging.debug("")
 
-                    else:
-
-                        location_charging_rate = charging_rates[row["TripEndLoc"]]
-                        logging.debug(f"Location charging rate: {location_charging_rate}")
-
-                        logging.debug(f"Charger available")
-                        SOC_percentage = SOC_list[-1]/battery_size
-                        charge_decision_prob = SOC_charging_prob(SOC_percentage)
-                        charge_decision = np.random.choice([0,1], p = [1-charge_decision_prob, charge_decision_prob])
-                        charge_decision_list.append(charge_decision)
-
-                        if charge_decision == 1:
-                            logging.debug("Individual has chosen to charge")
-
-                            charging_dict["IndividualID"].append(i)
-
-                            total_power_used = battery_size - SOC_list[-1]
-                            charge_start_time = row["TripEnd"]
-                            charge_end_time = charge_start_time + (total_power_used/location_charging_rate*60)
-
-                            # Rounding to nearest 5 minutes
-                            charge_end_time = 5 * round(charge_end_time / 5)
-
-                            charge_start_time_list.append(charge_start_time)
-                            charge_end_time_list.append(charge_end_time)
-                            total_power_used_list.append(total_power_used)
-
-                            SOC_list.append(np.round(battery_size,2))
-                            logging.debug(charge_decision_list)
-                            logging.debug(SOC_list)
-
-                            charging_dict["TotalPowerUsed"].append(total_power_used)
-                            charging_dict["ChargeStart"].append(charge_start_time)
-                            charging_dict["ChargeEnd"].append(charge_end_time)
-                            charging_dict["ChargeLoc"].append(row["TripEndLoc"])
-                            charging_dict["ChargingRate"].append(location_charging_rate)
-
-
-                        else:
-                            logging.debug("Individual has chosen NOT to charge")
-                            SOC_list.append(np.round(   SOC_list[-1], 2))
-                            logging.debug(charge_decision_list)
-                            logging.debug(SOC_list)
-
-            logging.info(SOC_list)
-            logging.info(charge_decision_list)
-            logging.info(total_power_used_list)
-            logging.info(charge_start_time_list)
-            logging.info(charge_end_time_list)
-
-
-            logging.info(i_df[["TripEndLoc", "IsCharger", "TripDisExSW", "Distance+1", "Req_charge+1", "TripStartRolling", "TripEndRolling", "TripStartRolling+1", "TripEndLoc", "TimeEndLoc"]].head())
-            print("")
+            logging.debug(charge_decision_list)
+            logging.debug(SOC_list)
+            
 
         charging_df = pd.DataFrame(charging_dict)
+        logging.info(f"Total trips: {total_trips}")
+        logging.info(f"Negative trips: {negative_trips}")
+        logging.info(f"% negative trips: {negative_trips/total_trips*100:.2f}%")
 
         return charging_df
-
-
-
-
-
-
-
 
         
 if __name__ == "__main__":
 
-    test = charging_logic(full_df, test_index=5)
+    test = charging_logic(full_df, test_index=100)
 
     test.to_csv(cfg.root_folder + "/output_csvs/charging_df_2017.csv", index=False)
---
+++ EVforecaster/charging_logic_auxillary.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import config as cfg
+import random
+import numpy as np
+import logging
+
+def generate_charger(x, home_charger_likelihood=0.96, work_charger_likelihood=0.62, public_charger_likelihood=0.17):
+    if x == 3:
+        return random.choices([1,0], weights=[home_charger_likelihood,1-home_charger_likelihood])[0]
+    if x == 2:
+        return random.choices([1,0], weights=[public_charger_likelihood,1-public_charger_likelihood])[0]
+    else:
+        return random.choices([1,0], weights=[work_charger_likelihood,1-work_charger_likelihood])[0]
+
+### Auxillary functions for charging logic ###
+
+def obtain_decision_to_charge(SOC, available_charger, time_duration_at_location, last_trip_flag,
+                       min_stop_time_to_charge,
+                       battery_size,
+                       SOC_charging_prob):
+    
+    if available_charger == 0:
+
+        logging.debug(f"No charger available")
+        return 0
+    
+    
+    elif time_duration_at_location < min_stop_time_to_charge and not last_trip_flag:
+
+        logging.debug(f"Insufficient time spent at location to charge")
+        return 0
+    
+    logging.debug("charger is available and car has stopped for sufficient time")
+    SOC_percentage = SOC/battery_size
+    charge_decision_prob = SOC_charging_prob(SOC_percentage)
+
+    logging.debug(f"charging decision prob: {charge_decision_prob}")
+
+    charge_decision = np.random.choice([0,1], p = [1-charge_decision_prob, charge_decision_prob])
+
+    logging.debug(f"charging decision: {charge_decision}")
+
+    return int(charge_decision)
+
+def calculate_charging_session(SOC, location_charging_rate, time_duration_at_location, last_trip_flag,
+                               charge_start_time, battery_size):
+
+    remaining_capacity = battery_size - SOC
+    logging.debug(f"Remaining capacity: {remaining_capacity:.2f}")
+
+    if last_trip_flag or pd.isna(time_duration_at_location):
+        logging.debug("Last trip or unknown duration — assume full charge")
+        charge_energy = remaining_capacity
+    else:
+        total_possible_charge = (time_duration_at_location / 60) * location_charging_rate
+        logging.debug(f"Total possible charge: {total_possible_charge:.2f}")
+        charge_energy = min(total_possible_charge, remaining_capacity)
+
+    new_SOC = SOC + charge_energy
+    charge_duration = (charge_energy / location_charging_rate) * 60  # in minutes
+    charge_end_time = charge_start_time + charge_duration
+
+    logging.debug(f"New SOC: {new_SOC}")
+    logging.debug(f"charge energy: {charge_energy}")
+    logging.debug(f"charge start time: {charge_start_time}")
+    logging.debug(f"charge end time: {charge_end_time}")
+    logging.debug(f"charge duration: {charge_duration}")   
+
+    return new_SOC, charge_energy, charge_start_time, charge_end_time, charge_duration
