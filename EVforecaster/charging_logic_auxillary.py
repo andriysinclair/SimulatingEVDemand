0eeb0ae3ce064ea9c9cdf15db2e2365e6ba53c89
Andriy Sinclair <andriysinclair@outlook.com>
2025-06-11 11:26:08 +0100

    Added docstrings

--- EVforecaster/charging_logic_auxillary.py
+++ EVforecaster/charging_logic_auxillary.py
@@ -5,7 +5,21 @@ import numpy as np
 import logging
 import config as cfg
 
-def generate_charger(x, home_charger_likelihood=cfg.charger_likelihood["home"], work_charger_likelihood=cfg.charger_likelihood["work"], public_charger_likelihood=cfg.charger_likelihood["other"]):
+def generate_charger(x, home_charger_likelihood=cfg.charger_likelihood["home"], work_charger_likelihood=cfg.charger_likelihood["work"], public_charger_likelihood=cfg.charger_likelihood["other"]) -> int:
+    """
+    generate_charger 
+
+    Generates a charger or no charger for a location with a given probability as defined in config.py 
+
+    Args:
+        x (int): location [1,2,3]
+        home_charger_likelihood (int, optional): probability [0,1]. Defaults to cfg.charger_likelihood["home"].
+        work_charger_likelihood (int, optional): prob [0,1]. Defaults to cfg.charger_likelihood["work"].
+        public_charger_likelihood (int, optional): prob [0,1]. Defaults to cfg.charger_likelihood["other"].
+
+    Returns:
+        int: A 1 or a 0 as obtained through probability
+    """    
     if x == 3:
         return random.choices([1,0], weights=[home_charger_likelihood,1-home_charger_likelihood])[0]
     if x == 2:
@@ -18,7 +32,24 @@ def generate_charger(x, home_charger_likelihood=cfg.charger_likelihood["home"], 
 def obtain_decision_to_charge(SOC, available_charger, time_duration_at_location, last_trip_flag,
                        min_stop_time_to_charge,
                        battery_size,
-                       SOC_charging_prob):
+                       SOC_charging_prob) -> int:
+    """
+    obtain_decision_to_charge 
+
+    Obtains a yes (1) or no (0) decision to charge based on SOC and other requirmenets such as minimum stop time
+
+    Args:
+        SOC (float): State of Charge (kW)
+        available_charger (int): available (1) not available (0)
+        time_duration_at_location (float): time at location
+        last_trip_flag (bool): is last trip?
+        min_stop_time_to_charge (float): in minutes
+        battery_size (float): in kW
+        SOC_charging_prob (func): function to determine charge probability based off SOC, available in config.py
+
+    Returns:
+        int: 1 (charge) or 0 (not charge)
+    """    
     
     if available_charger == 0:
 
@@ -33,10 +64,14 @@ def obtain_decision_to_charge(SOC, available_charger, time_duration_at_location,
     
     logging.debug("charger is available and car has stopped for sufficient time")
     SOC_percentage = SOC/battery_size
+
+    # Function calculates charging probabilites based off SOC
+
     charge_decision_prob = SOC_charging_prob(SOC_percentage)
 
     logging.debug(f"charging decision prob: {charge_decision_prob}")
 
+    # Draws 1,0 from charging decision probability
     charge_decision = np.random.choice([0,1], p = [1-charge_decision_prob, charge_decision_prob])
 
     logging.debug(f"charging decision: {charge_decision}")
@@ -44,25 +79,58 @@ def obtain_decision_to_charge(SOC, available_charger, time_duration_at_location,
     return int(charge_decision)
 
 def calculate_charging_session(SOC, location_charging_rate, time_duration_at_location, last_trip_flag,
-                               charge_start_time, battery_size):
+                               charge_start_time, battery_size)->tuple:
+    """
+    calculate_charging_session 
+
+    Creates a charging session if individual decides to charge.
+    Obtains charge extracted and the end of charging session.
+
+    Args:
+        SOC (float): State of charge [0, battery size]
+        location_charging_rate (float): in kWh
+        time_duration_at_location (float): in minutes
+        last_trip_flag (bool): True if this is individuals last trip of the week
+        charge_start_time (float): between 0 and 1440 (24hrs)
+        battery_size (float): kW
+
+    Returns:
+        tuple: returns 
+                - float, new SOC following charge
+                - float, energy obtained from charge (kW)
+                - float, charge start time [0,1440]
+                - float, charge end time [0,1440]
+                - float, charge duration (minutes)
+
+    """    
     
+    # Assuming individuals charge when they arrive at end location - this is charge start time
     charge_start_time = 5 * round(charge_start_time/5)
 
+    # Capacity at charging location
     remaining_capacity = battery_size - SOC
     logging.debug(f"Remaining capacity: {remaining_capacity:.2f}")
+
+    # If this is last trip there is no next trip for which to charge. Therefore if charging is chosen it will automatically
+    # result in a full charge
 
     if last_trip_flag or pd.isna(time_duration_at_location):
         logging.debug("Last trip or unknown duration — assume full charge")
         charge_energy = remaining_capacity
+
     else:
+        # If not last trip individual charges the minimum of a total possible charge given 
+        # time at location or their remaining capacity
         total_possible_charge = (time_duration_at_location / 60) * location_charging_rate
         logging.debug(f"Total possible charge: {total_possible_charge:.2f}")
         charge_energy = min(total_possible_charge, remaining_capacity)
 
+    # Adding the new charge to the SOC
     new_SOC = SOC + charge_energy
+
+    # Obtaining charge duration and charge end time
     charge_duration = (charge_energy / location_charging_rate) * 60  # in minutes
     charge_duration = 5 * round(charge_duration / 5)
-
     charge_end_time = charge_start_time + charge_duration
 
     logging.debug(f"New SOC: {new_SOC}")
